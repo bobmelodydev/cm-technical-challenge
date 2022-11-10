9c74a541f51f356316bde9ae685f04a4d04b5c39
Melody <bobmelodydev@gmail.com>
2022-11-10 13:00:23 -0500

    Initial version

--- handler.js
+++ handler.js
@@ -1,1 +1,8 @@
 // write your lambda here
+export const reverser = (stringToReverse) => {
+    const reversed = stringToReverse.reverse()
+    return {
+        payload: reversed,
+        message: "Thank you for using the reverser!",
+    }
+}
